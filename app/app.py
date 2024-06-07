60bf254faa43f8f49fe4ac1cdc3cf7d540d97d0b
Muzaffer Kahraman <muzaffer.kahraman@outlook.com>
2024-06-07 10:37:11 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def message():
-    return 'Welcome to'
+    return 'Welcome to my repo'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
